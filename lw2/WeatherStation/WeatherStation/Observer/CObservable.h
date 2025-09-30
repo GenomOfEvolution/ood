69aa9924ccdfe62be055999829bb7fcbf8ae0a84
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2025-09-30 20:55:41 +0300

    task3 - observers with priority

--- lw2/WeatherStation/WeatherStation/Observer/CObservable.h
+++ lw2/WeatherStation/WeatherStation/Observer/CObservable.h
@@ -2,8 +2,9 @@
 #include "IObservable.h"
 #include "IObserver.h"
 
-#include <set>
-#include <functional>
+#include <map>
+#include <unordered_map>
+#include <stdexcept>
 
 template <class T>
 class CObservable : public IObservable<T>
@@ -11,29 +12,44 @@ class CObservable : public IObservable<T>
 public:
 	using ObserverType = IObserver<T>;
 
-	void RegisterObserver(ObserverType& observer) override
+	void RegisterObserver(ObserverType& observer, int priority) override
 	{
-		m_observers.insert(&observer);
+		if (m_observerItMap.find(&observer) == m_observerItMap.end())
+		{
+			auto it = m_observers.emplace(priority, &observer);
+			m_observerItMap[&observer] = it;
+		}
+		else
+		{
+			throw std::runtime_error("Observer is already registered");
+		}
 	}
 
 	void NotifyObservers() override
 	{
 		T data = GetChangedData();
 		auto observers = m_observers;
+
 		for (auto& observer : observers)
 		{
-			observer->Update(data);
+			observer.second->Update(data);
 		}
 	}
 
 	void RemoveObserver(ObserverType& observer) override
 	{
-		m_observers.erase(&observer);
+		auto it = m_observerItMap.find(&observer);
+		if (it != m_observerItMap.end())
+		{
+			m_observers.erase(it->second);
+			m_observerItMap.erase(it);
+		}
 	}
 
 protected:
 	virtual T GetChangedData() const = 0;
 
 private:
-	std::set<ObserverType*> m_observers;
+	std::multimap<int, ObserverType*> m_observers;
+	std::unordered_map<ObserverType*, typename std::multimap<int, ObserverType*>::iterator> m_observerItMap;
 };
--- lw2/WeatherStation/WeatherStationTests/tests.cpp
+++ lw2/WeatherStation/WeatherStationTests/tests.cpp
@@ -34,7 +34,7 @@ TEST_CASE("Safely delete observer")
 
 			std::ostringstream os;
 			SelfDestructObserver observer(os, observable);
-			observable.RegisterObserver(observer);
+			observable.RegisterObserver(observer, 1);
 			
 			AND_GIVEN("a few dummy observers")
 			{
@@ -47,8 +47,8 @@ TEST_CASE("Safely delete observer")
 
 				CDummyObserver dummyObserver1;
 				CDummyObserver dummyObserver2;
-				observable.RegisterObserver(dummyObserver1);
-				observable.RegisterObserver(dummyObserver2);
+				observable.RegisterObserver(dummyObserver1, 2);
+				observable.RegisterObserver(dummyObserver2, 3);
 
 				WHEN("notifying observers")
 				{
@@ -58,6 +58,99 @@ TEST_CASE("Safely delete observer")
 					{
 						REQUIRE(os.str() == "I wasn`t deleted!");
 					}
+				}
+			}
+		}
+	}
+}
+
+
+TEST_CASE("observers with priority")
+{
+	GIVEN("an observable")
+	{
+		CWeatherData observable;
+
+		AND_GIVEN("observers #1 and #2")
+		{
+			class CObserver : public IObserver<SWeatherInfo>
+			{
+			public:
+				CObserver(std::ostream& out, int number)
+					: m_out(out)
+					, m_number(number)
+				{
+				}
+
+			private:
+				void Update(SWeatherInfo const&) override
+				{
+					m_out << "Observer #" << m_number << "\n";
+				}
+
+				std::ostream& m_out;
+				int m_number;
+			};
+
+			std::ostringstream out;
+			CObserver observer1(out, 1);
+			CObserver observer2(out, 2);
+
+			WHEN("adding #1 with priority 1 and #2 with priority 2 and notifying them")
+			{
+				observable.RegisterObserver(observer1, 1);
+				observable.RegisterObserver(observer2, 2);
+				observable.NotifyObservers();
+
+				THEN("#1 gets called first, then #2")
+				{
+					REQUIRE(out.str() == "Observer #1\nObserver #2\n");
+				}
+			}
+
+			WHEN("adding #1 with priority 5 and #2 with priority 1 and notifying them")
+			{
+				observable.RegisterObserver(observer1, 5);
+				observable.RegisterObserver(observer2, 1);
+				observable.NotifyObservers();
+
+				THEN("#2 gets called first, then #1")
+				{
+					REQUIRE(out.str() == "Observer #2\nObserver #1\n");
+				}
+			}
+
+			WHEN("adding both observers with the same priority (#1 first) and notifying them")
+			{
+				observable.RegisterObserver(observer1, 1);
+				observable.RegisterObserver(observer2, 1);
+				observable.NotifyObservers();
+
+				THEN("they are called in the order of registering: #1 gets called first, then #2")
+				{
+					REQUIRE(out.str() == "Observer #1\nObserver #2\n");
+				}
+			}
+
+			WHEN("adding both observers with the same priority (#2 first) and notifying them")
+			{
+				observable.RegisterObserver(observer2, 1);
+				observable.RegisterObserver(observer1, 1);
+				observable.NotifyObservers();
+
+				THEN("they are called in the order of registering: #2 gets called first, then #1")
+				{
+					REQUIRE(out.str() == "Observer #2\nObserver #1\n");
+				}
+			}
+
+			WHEN("adding single observer many times")
+			{
+				observable.RegisterObserver(observer1, 1);
+
+				THEN("observable throws error: already registred an observer")
+				{
+					REQUIRE_THROWS_AS(observable.RegisterObserver(observer1, 2), std::runtime_error);
 				}
 			}
 		}
