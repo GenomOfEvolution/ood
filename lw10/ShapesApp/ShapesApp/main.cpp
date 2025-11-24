af36ee4351d64f1c723d0cc241304488425a8ef0
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2025-11-24 10:36:15 +0300

    Инициализация проекта с Qt

---
+++ lw10/ShapesApp/ShapesApp/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <QApplication>
+#include <QLabel>
+#include <QWidget>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Проверка Qt");
+    window.resize(300, 150);
+
+    QLabel label("Привет, Qt работает! 🎉", &window);
+    label.move(70, 60);
+
+    window.show();
+
+    return app.exec();
+}
