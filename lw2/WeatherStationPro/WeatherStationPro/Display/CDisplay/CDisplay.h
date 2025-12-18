8a9c365a50dfbdfc9c993ae6dc7665149ff18347
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2025-12-18 14:21:43 +0300

    Removed unnecessary project

--- lw2/WeatherStationPro/WeatherStationPro/Display/CDisplay/CDisplay.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../Observer/IObserver.h"
-#include "../../WeatherDataPro/WeatherDataPro.h"
-
-class CDisplay : public IObserver<SWeatherInfo>
-{
-private:
-	void Update(SWeatherInfo const& data) override;
-};
